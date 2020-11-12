b85dad255fbc6f05a2685f75ed2843d9847732c5
Harshal Bhatia~ <2299-harshal.bhatia@users.noreply.source.golabs.io>
2020-11-12 12:16:46 +0530

    [Rohan|Harshal] WIP: Responsive without JS

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import React, {useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import { jsx, Grid, Box } from "theme-ui"
@@ -16,107 +16,105 @@ const Header = ({ siteTitle }) => {
     return () => window.removeEventListener('resize', updateWidth);
   }, []);
 
-  return ( 
+  return (
     <header
-    style={{
-      background: `#0e0e0e`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <Grid
-      columns={width>= 640? [ 3, "3fr 0.6fr 0.6fr"] : [ 1 ]}
-      sx={{
-        padding: "7vh 7vw 0vh 7vw",
-        paddingBottom: ["7vh", "7vh", "7vh", "7vh", "0vh"],
-        maxWidth: "100%",
+      style={{
+        background: `#0e0e0e`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <Box
+      <Grid
+        columns={[3, "minmax(640px, 3fr) 0.6fr 0.6fr"]}
         sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: width>=640? "left": "center"
+          padding: "7vh 7vw 0vh 7vw",
+          paddingBottom: ["7vh", "7vh", "7vh", "7vh", "0vh"],
+          maxWidth: "100%",
         }}
       >
-        <h2
-          sx={{ 
-            margin: 0,
-            fontSize: ["1.7rem", "2rem", "1.5rem", "2rem", "1.8rem"],
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: ["center", "center", "left", "left", "left"],
           }}
         >
-          <Link
-            to="/"
+          <h2
             sx={{
-              color: `#31C38B`,
-              textDecoration: `none`,
-              fontFamily: "body",
+              margin: 0,
+              fontSize: ["1.7rem", "2rem", "1.5rem", "2rem", "1.8rem"],
             }}
           >
-            {siteTitle}
-          </Link>
-        </h2>
-      </Box>
-      {width>= 640 && 
-      (
-        <>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "flex-end",
-        }}
-      >
-        <h3
-          sx={{
-            marginBottom: "0px",
-            color: "white",
-            fontFamily: "body",
-            fontSize: ["0.8rem", "0.8rem", "1rem", "1.2rem", "1.3rem"],
-          }}
-        >
-          <a
-            href="#description"
-            style={{
-              textDecoration: "none",
-              color: "white",
-            }}
-          >
-            About
-          </a>
-        </h3>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "flex-end",
-        }}
-      >
-        <h3
-          sx={{
-            marginBottom: "0px",
-            fontFamily: "body",
-            fontSize: ["0.8rem", "0.8rem", "1rem", "1.2rem", "1.3rem"],
-          }}
-        >
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/gojek/ziggurat"
-            style={{
-              color: "white",
-              textDecoration: "none",
-            }}
-          >
-            Source
-          </a>
-        </h3>
-      </Box>
-      </>
-      )
-        }
-    </Grid>
-  </header>
+            <Link
+              to="/"
+              sx={{
+                color: `#31C38B`,
+                textDecoration: `none`,
+                fontFamily: "body",
+              }}
+            >
+              {siteTitle}
+            </Link>
+          </h2>
+        </Box>
+        {width >= 640 && (
+          <>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              <h3
+                sx={{
+                  marginBottom: "0px",
+                  color: "white",
+                  fontFamily: "body",
+                  fontSize: ["0.8rem", "0.8rem", "1rem", "1.2rem", "1.3rem"],
+                }}
+              >
+                <a
+                  href="#description"
+                  style={{
+                    textDecoration: "none",
+                    color: "white",
+                  }}
+                >
+                  About
+                </a>
+              </h3>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
+              <h3
+                sx={{
+                  marginBottom: "0px",
+                  fontFamily: "body",
+                  fontSize: ["0.8rem", "0.8rem", "1rem", "1.2rem", "1.3rem"],
+                }}
+              >
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/gojek/ziggurat"
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                  }}
+                >
+                  Source
+                </a>
+              </h3>
+            </Box>
+          </>
+        )}
+      </Grid>
+    </header>
   )
 }
 
